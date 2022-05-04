9e8f8b8717ba5887a6251967f7f6b1c85e5b75d6
Sudhir swain <70561891+sudhir192311@users.noreply.github.com>
2022-05-04 18:53:19 +0530

    Update mysql.py

--- mysql.py
+++ mysql.py
@@ -5,3 +5,13 @@ conn=mysql.connector.connect(host='127.0.0.1',database='sudhir',user='root',pass
 cursor=conn.cursor()
 cursor.execute('insert into user values (04,"pratap","chilika");')
 # insert values by using cursor
+cursor.execute("select * from user")
+for i in cursor:
+  print(i)
+# print all data in user table  
+#and fetcth all data 
+result=cursor.fetchall()
+or result=cursor.fetchone()
+for i in result:
+  print(i)
+  
