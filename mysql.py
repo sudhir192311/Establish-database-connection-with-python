751e3ae162799ffd9fc38510cb3a7775754ec149
Sudhir swain <70561891+sudhir192311@users.noreply.github.com>
2022-05-04 15:52:49 +0530

    Update mysql.py

--- mysql.py
+++ mysql.py
@@ -1,5 +1,7 @@
 import mysql.connector
 # package mysql.connector
 conn=mysql.connector.connect(host='127.0.0.1',database='sudhir',user='root',password='')
+#crete cursor with the help of conn obj
 cursor=conn.cursor()
 cursor.execute('insert into user values (04,"pratap","chilika");')
+# insert values by using cursor
